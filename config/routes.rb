18a5d684c17f624a2956d90b6a2e2aa2f1780f6a
Anton Moroz <anton.moroz@helsinki.fi>
2018-09-11 18:52:24 +0300

    2. viikon 1. tehtävä (esimerkkeineen) done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   resources :beers
   resources :breweries
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get('/', to: 'breweries#index')
+  get('ratings', to: 'ratings#index')
+  #vaihtoehtoisesti - root 'breweries#index'
+  # get 'kaljat', to: 'beers#index'
 end
