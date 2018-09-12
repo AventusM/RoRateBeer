c24c86b29a1f3fe0f2120ec98f12b848b8629b92
Anton Moroz <anton.moroz@helsinki.fi>
2018-09-12 16:06:11 +0300

    Viikko 2, tehtävä 3.

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,16 @@
 class Beer < ApplicationRecord
   belongs_to :brewery
   has_many :ratings
+
+  def print_report
+    #self = this
+    puts "Beer name: #{self.name}"
+    puts "Beer style: #{self.style}"
+    puts "Produced by #{self.brewery.name}"
+  end
+
+  def change
+    self.style = "Cider"
+    puts "Bewerage style is now #{self.style}"
+  end
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,14 @@
 class Rating < ApplicationRecord
   belongs_to :beer
+
+  # "puts" helvettii täältä nii alkaa näkyy jo jotain -.-
+  def to_s
+    # beer_by_beer_id = Beer.find_by(id: self.beer_id)
+
+    # Unohdin rails-taian ...
+    # Rating has one beer -> self.beer.param
+    "#{self.beer.name} #{self.score}"
+  end
+
 end
+
