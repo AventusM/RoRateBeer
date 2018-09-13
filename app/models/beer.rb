1e94ddf2b50276f731c1eab7c7ad0333435acaff
Anton Moroz <anton.moroz@helsinki.fi>
2018-09-13 17:25:57 +0300

    Viikko 2, tehtävät 5 ja 6 yhdessä paketissa (5. kommentoitu pois).

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,10 +3,19 @@ class Beer < ApplicationRecord
   has_many :ratings
 
   def average_rating
-    sum = 0
-    self.ratings.each{ |rating| sum += rating.score }
-    average = sum / (self.ratings.count * 1.0)
-    "Has #{self.ratings.count} ratings, average #{average}"
+    # YHTEINEN
+    ratings_amount = self.ratings.count
+
+    # TEHTÄVÄ 5
+    # sum = 0
+    # self.ratings.each{ |rating| sum+=rating.score } # Calculate sum
+    # average = sum / (ratings_amount * 1.0)
+    # "Has #{ratings_amount} ratings, average #{average}"
+
+    # TEHTÄVÄ 6
+    # No reduce for object straight up - have to use inject instead
+    average = self.ratings.inject(0.0){|sum,n| sum + n.score} / ratings_amount # use 0.0 as starter to have floats instead of multiplying by 1.0, sum is accumulator, n is a singular rating
+    "Has #{ratings_amount} " + "rating".pluralize(ratings_amount) + " average #{average}" # Tehtävä 6
   end
 
   def print_report
