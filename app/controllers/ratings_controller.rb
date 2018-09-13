51c8effaf5cc1e579b0333e9c5dfab6237802360
Anton Moroz <anton.moroz@helsinki.fi>
2018-09-13 20:55:56 +0300

    2. viikko, asiat tehty 7. tehtävän loppuun asti.

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -3,4 +3,22 @@ class RatingsController < ApplicationController
     @ratings = Rating.all
     render :index
   end
+
+  def new
+    @rating = Rating.new
+  end
+
+  def create
+   # raise # palauttaa paramit errorsivulla
+   # byebug # muuttuja rating = params[:rating] -> rating[:score] tai rating[:beer_id] -- vaihtoehtoisesti params[:rating][:score] ym.
+   # binding.pry -- vaihtoehto byebugille
+   # Rating.create(params[:rating]) -- ei kelpaa
+   Rating.create(params.require(:rating).permit(:beer_id, :score))
+   # Redirect luonnin jälkeen
+   redirect_to(ratings_path)
+
+
+   # redirect_to("http://www.cs.helsinki.fi") xd
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,16 @@ Rails.application.routes.draw do
   resources :breweries
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # ROOT
   get('/', to: 'breweries#index')
+
+  # REST Rating
+  # GET
   get('ratings', to: 'ratings#index')
+  get('ratings/new', to: 'ratings#new')
+  # POST
+  post('ratings', to: 'ratings#create')
+
   #vaihtoehtoisesti - root 'breweries#index'
   # get 'kaljat', to: 'beers#index'
 end
