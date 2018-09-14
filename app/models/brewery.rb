ce5ae94b557b1001996f9e5272b639ffa7c1ea46
Anton Moroz <anton.moroz@helsinki.fi>
2018-09-14 22:27:49 +0300

    Viikko 2, 14. tehtävä tehty esimerkin ja 6. tehtävän mallin mukaisesti.

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,10 @@
 class Brewery < ApplicationRecord
   has_many :beers, dependent: :destroy # Panimoon liittyvät oluet poistetaan oluen poistamisessa (+ siihen liittyvät ratingit)
+  has_many :ratings, through: :beers # Nyt myös panimo voi hyödyntää OMIEN OLUIDEN ratingeja (käytetään suoraan olut = Find_by(...) -> olut.ratings.count tms.)
+
+  def average_rating
+    ratings_amount = self.ratings.count # Tässä kuitenkin tulee KAIKKIEN OLUIDEN ratingimäärä
+    average = self.ratings.inject(0.0){|sum,n| sum + n.score} / ratings_amount # use 0.0 as starter to have floats instead of multiplying by 1.0, sum is accumulator, n is a singular rating
+    "Has #{ratings_amount} " + "rating".pluralize(ratings_amount) + ", average #{average}" # Tehtävä 6
+  end
 end
