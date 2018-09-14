a7344e26d382ec2109fe1617ea5f96bc8b85f19d
Anton Moroz <anton.moroz@helsinki.fi>
2018-09-14 18:39:54 +0300

    Viikko 2, 9. tehtävä tehty.

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -15,6 +15,7 @@ class BeersController < ApplicationController
   # GET /beers/new
   def new
     @beer = Beer.new
+    @breweries = Brewery.all
   end
 
   # GET /beers/1/edit
@@ -28,7 +29,7 @@ class BeersController < ApplicationController
 
     respond_to do |format|
       if @beer.save
-        format.html { redirect_to @beer, notice: 'Beer was successfully created.' }
+        format.html { redirect_to beers_path, notice: 'Beer was successfully created.' } # ei voi laittaa sulkeisiin ( ts. redirect_to(beers_path) ) --> johtunee format.html:n ominaisuuksista
         format.json { render :show, status: :created, location: @beer }
       else
         format.html { render :new }
