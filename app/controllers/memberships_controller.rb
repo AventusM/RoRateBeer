95889db3287fb6e34004e1a72ab3dc444f792ef7
Anton Moroz <anton.moroz@helsinki.fi>
2018-09-19 16:36:57 +0300

    Viikko 3, 11. tehtävä tuntuu olevan tehty. Ilman testejä tätä on kuitenkin hankala varmistaa, tutkivan ja hutkivan testauksen yritykset tuhota sovellus eivät enää toimi, joten luottamusta löytyy...

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -4,18 +4,25 @@ class MembershipsController < ApplicationController
 
   def new
     @membership = Membership.new
-    @beer_clubs = BeerClub.all
+    all_clubs = BeerClub.all
+    user_clubs = User.find(session[:user_id]).beer_clubs
+    # raise
+    @beer_clubs = all_clubs - user_clubs # https://stackoverflow.com/questions/32436091/filter-to-exclude-elements-from-array
     render :new
   end
 
   def create
-    @membership = Membership.create(params.require(:membership).permit(:beer_club_id))
-    @membership.user_id = current_user.id
-    if(@membership.save)
+    user_clubs = User.find(session[:user_id]).beer_clubs
+    # Redirect etusivulle (breweries_pathin voisi laittaa rootiksi..), jos valitaan tyhjä TAI KIERRETÄÄN valinta client-sidelta, ehkä lisättävä notifikaatio?
+    # Tällöin redirect muutettava toiseen muotoon (eli rubymainen if --> if{}then render this page with nofitications...)
+    if(params[:membership].nil? || user_clubs.include?(BeerClub.find(params[:membership][:beer_club_id]))) # vastattu tyhjään tai client-side juttu kierretty..?
+      return redirect_to(breweries_path)
+    else
+      @membership = Membership.create(params.require(:membership).permit(:beer_club_id))
+      @membership.user_id = current_user.id
+      @membership.save
       redirect_to(user_path(current_user.id))
     end
-
     # Redirect luonnin jälkeen noticen kanssa?
-    # raise
   end
 end
