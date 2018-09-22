a80eb9a26485c8455564a26ffcd4788b0d4b0396
Anton Moroz <anton.moroz@helsinki.fi>
2018-09-22 17:36:37 +0300

    Viikko 3, 15. tehtävä tehty.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,13 @@ class User < ApplicationRecord
   validates :username, uniqueness: true, length: {minimum: 3, maximum: 30} # Tällä validoidaan uniikit käyttäjätunnukset, joiden minimipituus on 3
   validates :password, length: {minimum: 4}, format: { with: /^(?=.*[A-Z])(?=.*[0-9])/, :multiline => true } # Salasanan pituus väh. 4 merkkiä, sisältää vähintään yhden ison kirjaimen sekä vähintään yhden numeron
 
-  has_many :ratings # Käyttäjällä voi olla monta arvostelua -- access by user.ratings.params
+  # dependent - oliot poistetaan, jos niihin liittyvä käyttäjä poistetaan
+  # dependent - oliot poistetaan, jos niihin liittyvä käyttäjä poistetaan
+  # dependent - oliot poistetaan, jos niihin liittyvä käyttäjä poistetaan
+  
+  has_many :ratings, dependent: :destroy # Käyttäjällä voi olla monta arvostelua -- access by user.ratings.params
   has_many :beers, through: :ratings # Monesta-moneen-yhteys arvostelujen kautta oluihin
   
-  has_many :memberships # Käyttäjä voi liittyä moneen klubiin
+  has_many :memberships, dependent: :destroy # Käyttäjä voi liittyä moneen klubiin
   has_many :beer_clubs, through: :memberships
 end
