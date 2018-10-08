32257a570584a9a6730b754a45a4314f979b3e33
Anton Moroz <anton.moroz@helsinki.fi>
2018-10-08 16:21:47 +0300

    Lisätty viikolta 5 puuttuvat ominaisuudet.

--- app/controllers/beer_clubs_controller.rb
+++ app/controllers/beer_clubs_controller.rb
@@ -1,6 +1,89 @@
+# class BeerClubsController < ApplicationController
+#   before_action :ensure_that_signed_in, except: [:index, :show]
+#   before_action :set_beer_club, only: [:show, :edit, :update, :destroy]
+
+#   # GET /beer_clubs
+#   # GET /beer_clubs.json
+#   def index
+#     @beer_clubs = BeerClub.all
+#   end
+
+#   # GET /beer_clubs/1
+#   # GET /beer_clubs/1.json
+#   def show
+#     @membership = Membership.new
+#     @membership.beer_club = @beer_club
+#     # raise
+
+#     # Nimitys user tulee membership - modelista
+#     # TURHA - create - metodi liittää käyttäjän jäsenyyteen myöhemmin
+#     # @membership.user = current_user
+#     # raise
+#   end
+
+#   # GET /beer_clubs/new
+#   def new
+#     @beer_club = BeerClub.new
+#   end
+
+#   # GET /beer_clubs/1/edit
+#   def edit
+#   end
+
+#   # POST /beer_clubs
+#   # POST /beer_clubs.json
+#   def create
+#     @beer_club = BeerClub.new(beer_club_params)
+
+#     respond_to do |format|
+#       if @beer_club.save
+#         format.html { redirect_to @beer_club, notice: 'Beer club was successfully created.' }
+#         format.json { render :show, status: :created, location: @beer_club }
+#       else
+#         format.html { render :new }
+#         format.json { render json: @beer_club.errors, status: :unprocessable_entity }
+#       end
+#     end
+#   end
+
+#   # PATCH/PUT /beer_clubs/1
+#   # PATCH/PUT /beer_clubs/1.json
+#   def update
+#     respond_to do |format|
+#       if @beer_club.update(beer_club_params)
+#         format.html { redirect_to @beer_club, notice: 'Beer club was successfully updated.' }
+#         format.json { render :show, status: :ok, location: @beer_club }
+#       else
+#         format.html { render :edit }
+#         format.json { render json: @beer_club.errors, status: :unprocessable_entity }
+#       end
+#     end
+#   end
+
+#   # DELETE /beer_clubs/1
+#   # DELETE /beer_clubs/1.json
+#   def destroy
+#     @beer_club.destroy
+#     respond_to do |format|
+#       format.html { redirect_to beer_clubs_url, notice: 'Beer club was successfully destroyed.' }
+#       format.json { head :no_content }
+#     end
+#   end
+
+#   private
+#     # Use callbacks to share common setup or constraints between actions.
+#     def set_beer_club
+#       @beer_club = BeerClub.find(params[:id])
+#     end
+
+#     # Never trust parameters from the scary internet, only allow the white list through.
+#     def beer_club_params
+#       params.require(:beer_club).permit(:name, :founded, :city)
+#     end
+# end
 class BeerClubsController < ApplicationController
-  before_action :ensure_that_signed_in, except: [:index, :show]
   before_action :set_beer_club, only: [:show, :edit, :update, :destroy]
+  before_action :ensure_that_signed_in, except: [:index, :show]
 
   # GET /beer_clubs
   # GET /beer_clubs.json
@@ -11,14 +94,11 @@ class BeerClubsController < ApplicationController
   # GET /beer_clubs/1
   # GET /beer_clubs/1.json
   def show
-    @membership = Membership.new
-    @membership.beer_club = @beer_club
-    # raise
-
-    # Nimitys user tulee membership - modelista
-    # TURHA - create - metodi liittää käyttäjän jäsenyyteen myöhemmin
-    # @membership.user = current_user
-    # raise
+    @membership = if @beer_club.members.include? current_user
+                    @beer_club.memberships.where(user: current_user).first
+                  else
+                    Membership.new beer_club: @beer_club
+                  end
   end
 
   # GET /beer_clubs/new
@@ -71,13 +151,14 @@ class BeerClubsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_beer_club
-      @beer_club = BeerClub.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def beer_club_params
-      params.require(:beer_club).permit(:name, :founded, :city)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_beer_club
+    @beer_club = BeerClub.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def beer_club_params
+    params.require(:beer_club).permit(:name, :founded, :city)
+  end
 end
--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,34 +1,112 @@
-class MembershipsController < ApplicationController
-  # Olemassaoleva user id sessiosta - redirect jos ei ole + notice?
-  # Param club id formista
+# class MembershipsController < ApplicationController
+#   # Olemassaoleva user id sessiosta - redirect jos ei ole + notice?
+#   # Param club id formista
 
+#   def new
+#     @membership = Membership.new
+#     all_clubs = BeerClub.all
+#     user_clubs = User.find(session[:user_id]).beer_clubs
+#     # raise
+#     @beer_clubs = all_clubs - user_clubs # https://stackoverflow.com/questions/32436091/filter-to-exclude-elements-from-array
+#     render :new
+#   end
+
+#   def create
+#     user_clubs = User.find(session[:user_id]).beer_clubs
+#     # Redirect etusivulle (breweries_pathin voisi laittaa rootiksi..), jos valitaan tyhjä TAI KIERRETÄÄN valinta client-sidelta, ehkä lisättävä notifikaatio?
+#     # Tällöin redirect muutettava toiseen muotoon (eli rubymainen if --> if{}then render this page with nofitications...)
+#     if(params[:membership].nil? || user_clubs.include?(BeerClub.find(params[:membership][:beer_club_id]))) # vastattu tyhjään tai client-side juttu kierretty..?
+#       return redirect_to(breweries_path)
+#     else
+#       @membership = Membership.create(params.require(:membership).permit(:beer_club_id)) # fieldin id:n tulee olla beer_club_id
+#       @membership.user_id = current_user.id
+#       @membership.save
+#       redirect_to(beer_club_path(@membership.beer_club_id), notice: 'Welcome to the club, ' + current_user.username + '!')
+#       # redirect_to(user_path(current_user.id))
+#     end
+#     # Redirect luonnin jälkeen noticen kanssa?
+#   end
+
+#   def destroy
+#     # @membership = current_user.id
+#     raise
+#   end
+# end
+class MembershipsController < ApplicationController
+  before_action :set_membership, only: [:show, :edit, :update, :destroy]
+
+  # GET /memberships
+  # GET /memberships.json
+  def index
+    @memberships = Membership.all
+  end
+
+  # GET /memberships/1
+  # GET /memberships/1.json
+  def show
+  end
+
+  # GET /memberships/new
   def new
     @membership = Membership.new
-    all_clubs = BeerClub.all
-    user_clubs = User.find(session[:user_id]).beer_clubs
-    # raise
-    @beer_clubs = all_clubs - user_clubs # https://stackoverflow.com/questions/32436091/filter-to-exclude-elements-from-array
-    render :new
+    @beer_clubs = BeerClub.all - current_user.beer_clubs
   end
 
+  # GET /memberships/1/edit
+  def edit
+  end
+
+  # POST /memberships
+  # POST /memberships.json
   def create
-    user_clubs = User.find(session[:user_id]).beer_clubs
-    # Redirect etusivulle (breweries_pathin voisi laittaa rootiksi..), jos valitaan tyhjä TAI KIERRETÄÄN valinta client-sidelta, ehkä lisättävä notifikaatio?
-    # Tällöin redirect muutettava toiseen muotoon (eli rubymainen if --> if{}then render this page with nofitications...)
-    if(params[:membership].nil? || user_clubs.include?(BeerClub.find(params[:membership][:beer_club_id]))) # vastattu tyhjään tai client-side juttu kierretty..?
-      return redirect_to(breweries_path)
-    else
-      @membership = Membership.create(params.require(:membership).permit(:beer_club_id)) # fieldin id:n tulee olla beer_club_id
-      @membership.user_id = current_user.id
-      @membership.save
-      redirect_to(beer_club_path(@membership.beer_club_id), notice: 'Welcome to the club, ' + current_user.username + '!')
-      # redirect_to(user_path(current_user.id))
+    @membership = Membership.new(membership_params)
+    @membership.user = current_user
+
+    respond_to do |format|
+      if @membership.save
+        format.html { redirect_to @membership.beer_club, notice: "#{current_user.username} welcome to the club!" }
+        format.json { render :show, status: :created, location: @membership }
+      else
+        @beer_clubs = BeerClub.all - current_user.beer_clubs
+        format.html { render :new }
+        format.json { render json: @membership.errors, status: :unprocessable_entity }
+      end
     end
-    # Redirect luonnin jälkeen noticen kanssa?
   end
 
+  # PATCH/PUT /memberships/1
+  # PATCH/PUT /memberships/1.json
+  def update
+    respond_to do |format|
+      if @membership.update(membership_params)
+        format.html { redirect_to @membership, notice: 'Membership was successfully updated.' }
+        format.json { render :show, status: :ok, location: @membership }
+      else
+        format.html { render :edit }
+        format.json { render json: @membership.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /memberships/1
+  # DELETE /memberships/1.json
   def destroy
-    # @membership = current_user.id
-    raise
+    @membership.destroy
+    respond_to do |format|
+      format.html { redirect_to @membership.user, notice: "Membership in #{@membership.beer_club.name} ended" }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_membership
+    @membership = Membership.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def membership_params
+    params.require(:membership).permit(:beer_club_id, :user_id)
   end
 end
