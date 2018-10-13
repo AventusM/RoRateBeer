aa077aa785aac8ebee7fe6544c014d8690f6818a
Anton Moroz <anton.moroz@helsinki.fi>
2018-10-13 11:35:47 +0300

    Viikko 6, 9. tehtävä tehty (simppeli ratkaisu esimerkkeihin verrattuna kuitenkin...)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def edit_button_if_current_user(item)
+    unless current_user.nil?
+      edit = link_to('Edit', url_for([:edit], item, class: 'btn btn-link btn-sm'))
+      raw('#{edit}')
+    end
+  end
+
+  def destroy_button_if_current_user(item)
+    unless current_user.nil?
+      del = link_to('Destroy', item, method: :delete, data: {confirm: 'Are you sure?'}, class: 'btn btn-danger btn-link btn-sm')
+      raw('#{del}')
+    end
+  end
+
+  def round(average)
+    number_with_precision(average, precision: 1)
+  end
 end
--- app/models/concerns/rating_average.rb
+++ app/models/concerns/rating_average.rb
@@ -7,7 +7,6 @@ module RatingAverage
     # Ei voida jakaa nollalla (NaN)
     return 0 if ratings_amount == 0
     average = ratings.inject(0.0){ |sum, n| sum + n.score } / ratings_amount # use 0.0 as starter to have floats instead of multiplying by 1.0. Sum is accumulator and n is a singular rating
-    # "Has #{ratings_amount} " + "rating".pluralize(ratings_amount) + ", average #{average}" # Tehtävä 6
-    average.round(1) # Saattaa rikkoa hieman ulkonäköä juuri nyt.. PYÖRISTETTY HIEMAN !!!
+    average
   end
 end
