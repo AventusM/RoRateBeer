831fee7d01759127955b864e9bdcc645487638dd
Anton Moroz <anton.moroz@helsinki.fi>
2018-10-16 17:57:25 +0300

    Viikko 7, 5. tehtävä tehty.

--- spec/features/beerlist_page_spec.rb
+++ spec/features/beerlist_page_spec.rb
@@ -30,12 +30,41 @@ describe "Beerlist page" do
   it("should show beers in alphabet order by default", js:true) do
     visit(beerlist_path)
 
-    first_name_row = find('table').find('tr:nth-child(2)').find('td:nth-child(1)')
-    second_name_row = find('table').find('tr:nth-child(3)').find('td:nth-child(1)')
-    third_name_row = find('table').find('tr:nth-child(4)').find('td:nth-child(1)')
-    
-    expect(first_name_row).to(have_content("Fastenbier"))
-    expect(second_name_row).to(have_content("Lechte Weisse"))
-    expect(third_name_row).to(have_content("Nikolai"))
+    first_name = find('table').find('tr:nth-child(2)').find('td:nth-child(1)')
+    second_name = find('table').find('tr:nth-child(3)').find('td:nth-child(1)')
+    third_name = find('table').find('tr:nth-child(4)').find('td:nth-child(1)')
+
+    expect(first_name).to(have_content("Fastenbier"))
+    expect(second_name).to(have_content("Lechte Weisse"))
+    expect(third_name).to(have_content("Nikolai"))
+  end
+
+  it("shows beers in styles' alphabetic order when clicked on style link", js:true) do
+    visit(beerlist_path)
+    # save_and_open_page
+    click_link("Style")
+    # save_and_open_page
+
+    first_style = find('table').find('tr:nth-child(2)').find('td:nth-child(2)')
+    second_style = find('table').find('tr:nth-child(3)').find('td:nth-child(2)')
+    third_style = find('table').find('tr:nth-child(4)').find('td:nth-child(2)')
+
+    expect(first_style).to(have_content("Lager"))
+    expect(second_style).to(have_content("Rauchbier"))
+    expect(third_style).to(have_content("Weizen"))
+  end
+
+  it("shows beers in breweries' alphabetic order when clicked on brewery link", js:true) do
+    visit(beerlist_path)
+    # save_and_open_page
+    click_link("Brewery")
+
+    first_brewery = find('table').find('tr:nth-child(2)').find('td:nth-child(3)')
+    second_brewery = find('table').find('tr:nth-child(3)').find('td:nth-child(3)')
+    third_brewery = find('table').find('tr:nth-child(4)').find('td:nth-child(3)')
+
+    expect(first_brewery).to(have_content("Ayinger"))
+    expect(second_brewery).to(have_content("Koff"))
+    expect(third_brewery).to(have_content("Schlenkerla"))
   end
 end
